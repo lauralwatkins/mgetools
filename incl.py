22ab79ee5baf426f3073375036a7016c43103c9b
Laura L Watkins <lauralwatkins@gmail.com>
2014-01-21 17:57:45 -0500

    MGE: inclination calculator returns pi/2 in spherical case (q=1).

--- incl.py
+++ incl.py
@@ -13,11 +13,12 @@
 #     v1.0 : Laura L Watkins [lauralwatkins@gmail.com] - MPIA, 2012/06/19
 # -----------------------------------------------------------------------------
 
-from numpy import arccos, rad2deg, sqrt
+from numpy import arccos, pi, rad2deg, sqrt
 
 
-def incl( p, q, deg=False ):
+def incl( p, q ):
     
-    i = arccos( sqrt( ( p**2 - q**2 ) / ( 1. - q**2 ) ) )
+    if q == 1.: i = pi / 2.
+    else: i = arccos( sqrt( ( p**2 - q**2 ) / ( 1. - q**2 ) ) )
     
     return i
