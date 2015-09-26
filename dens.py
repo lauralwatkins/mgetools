Fix surf and dens to handle units.

dens.py
@@ -13,13 +13,21 @@ def dens(imge, r, z):
     Calculates the volume density of an MGE at given (R,z).
     
     INPUTS
-      imge : intrinsic MGE
-      r    : intrinsic R 
-      z    : intrinsic z
+      imge : intrinsic MGE, must be given as an astropy Table or QTable
+      r    : intrinsic R, should have the same units as MGE widths
+      z    : intrinsic z, should have the same units as MGE widths
     """
     
-    cpts = np.array([ cpt["i"]*np.exp(-0.5/cpt["s"]**2*(r**2+(z/cpt["q"])**2))\
-        for cpt in imge ])
+    try:
+        r.to(imge["s"].unit)
+        z.to(imge["s"].unit)
+    except:
+        print "MGE.SURF: cannot convert r and z units to MGE width units."
+        return np.nan
+    
+    cpts = np.array([ cpt["i"]*np.exp(-0.5/cpt["s"]**2 \
+        *(r.to(imge["s"].unit)**2+(z.to(imge["s"].unit)/cpt["q"])**2)) \
+        for cpt in imge ])*imge["i"].unit
     dens = np.sum(cpts, axis=0)
     
     return dens

surf.py
@@ -13,13 +13,21 @@ def surf(pmge, x, y):
     Calculates the surface density of an MGE at given (x',y').
     
     INPUTS
-      pmge : projected MGE
-      x    : projected x'
-      y    : projected y'
+      pmge : projected MGE, must be given as an astropy Table or QTable
+      x    : projected x', should have the same units as MGE widths
+      y    : projected y', should have the same units as MGE widths
     """
     
-    cpts = np.array([ cpt["i"]*np.exp(-0.5/cpt["s"]**2*(x**2+(y/cpt["q"])**2))\
-        for cpt in pmge ])
+    try:
+        x.to(pmge["s"].unit)
+        y.to(pmge["s"].unit)
+    except:
+        print "MGE.SURF: cannot convert x and y units to MGE width units."
+        return np.nan
+    
+    cpts = np.array([ cpt["i"]*np.exp(-0.5/cpt["s"]**2 \
+        *(x.to(pmge["s"].unit)**2+(y.to(pmge["s"].unit)/cpt["q"])**2)) \
+        for cpt in pmge ])*pmge["i"].unit
     surf = np.sum(cpts, axis=0)
     
     return surf