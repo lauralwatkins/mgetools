599beb624b08e0be75d0a7b1c9b4c21fb2b69539
Laura L Watkins <lauralwatkins@gmail.com>
2019-02-01 12:16:06 +0100

    Tidy up projmass and intrmass.

--- intrmass.py
+++ intrmass.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-# -----------------------------------------------------------------------------
-# MGE.INTRMASS
-# Glenn van de Ven [glenn@mpia.de]
-# Laura L Watkins [lauralwatkins@gmail.com]
-# -----------------------------------------------------------------------------
 
 import numpy as np
 from astropy import units as u
@@ -11,7 +6,7 @@ from scipy import special
 from scipy.integrate import quad
 
 
-def intg_intrmass(theta, r, imge):
+def IntegrandCumulative3D(theta, r, imge):
     
     scq = np.sqrt(np.sin(theta)**2 + np.cos(theta)**2 / imge["q"]**2)
     x = r*scq/imge["s"]/np.sqrt(2)
@@ -23,35 +18,39 @@ def intg_intrmass(theta, r, imge):
     return res
 
 
-def intrmass(r, imge, ellrad=False):
+def Cumulative3D(r, imge, ellrad=False):
     
     """
-    Calculates mass enclosed within intrinsic radius for an intrinsic MGE.
+    Calculates cumulative profile from a 3-dimensional (deprojected) MGE.
     
     INPUTS
       r    : intrinsic radius
       imge : intrinsic mge
     
-    OPTIONS
+    OPTIONAL KEYWORDS
       ellrad : calculate within elliptic radius (only for constant flattening)
     """
     
     res = np.zeros(len(r))*imge["i"].unit*imge["s"].unit**3
     
-    if ellrad:
-        # within ellipsoidal radius for *constant* flattening
-        if imge["q"].std()!=0: print("MGE.INTRMASS WARNING: you selected " \
-            + "elliptical radius but your flattening is not constant.")
+    # within ellipsoidal radius for *constant* flattening or no flattening
+    if ellrad or not "q" in imge.colnames or np.all(imge["q"]==1):
+        
+        if "q" in imge.colnames and imge["q"].ptp()>0:
+            print("MGE.Cumulative3D: you selected elliptical radius but " \
+                + "your flattening is not constant.")
+            return
+        
         for i in range(len(r)):
             x  = r[i]/np.sqrt(2)/imge["s"]
             intg = np.sqrt(np.pi/2)*special.erf(x.value) \
                 - np.sqrt(2)*x*np.exp(-x**2)
             res[i] = 4*np.pi*np.sum(imge["s"]**3*imge["q"]*imge["i"]*intg)
     
+    # within spherical radius
     else:
-        # within spherical radius
         for i in range(len(r)):
-            res[i] = 4*np.pi*quad(lambda x: intg_intrmass(x, r[i], imge), \
-                0, np.pi/2)[0]*imge["i"].unit*imge["s"].unit**3
+            res[i] = 4*np.pi*quad(lambda x: IntegrandCumulative3D(x, r[i],
+                imge), 0, np.pi/2)[0]*imge["i"].unit*imge["s"].unit**3
     
     return res
--- projmass.py
+++ projmass.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
-# -----------------------------------------------------------------------------
-# MGE.PROJMASS
-# Glenn van de Ven [glenn@mpia.de]
-# Laura L Watkins [lauralwatkins@gmail.com]
-# -----------------------------------------------------------------------------
 
 import numpy as np
 from astropy import units as u
 from scipy.integrate import quad
 
 
-def intg_projmass(phi, R, pmge):
+def IntegrandCumulative2D(phi, R, pmge):
     
     pjph = (1 + (1/pmge["q"]**2 - 1)*np.sin(phi)**2) / pmge["s"]**2
     res = np.sum(pmge["i"]*(1-np.exp(-0.5*R**2*pjph))/pjph).value
@@ -18,33 +13,37 @@ def intg_projmass(phi, R, pmge):
     return res
 
 
-def projmass(R, pmge, ellrad=False):
+def Cumulative2D(R, pmge, ellrad=False):
     
     """
-    Calculates mass enclosed within projected radius for a projected MGE.
+    Calculates cumulative profile from a 2-dimensional (projected) MGE.
     
     INPUTS
       R    : projected radius
       pmge : projected mge
     
-    OPTIONS
+    OPTIONAL KEYWORDS
       ellrad : calculate within elliptic radius (only for constant flattening)
     """
     
     res = np.zeros(len(R))*pmge["i"].unit*pmge["s"].unit**2
     
-    if ellrad:
-        # within elliptic radius for *constant* flattening 
-        if pmge["q"].std()!=0: print("MGE.PROJMASS WARNING: you selected " \
-            + "elliptical radius but your flattening is not constant.")
+    # within ellipsoidal radius for *constant* flattening or no flattening
+    if ellrad or not "q" in pmge.colnames or np.all(pmge["q"]==1):
+        
+        if "q" in pmge.colnames and pmge["q"].std()!=0:
+            print("MGE.Cumulative2D: you selected elliptical radius but " \
+                + "your flattening is not constant.")
+            return
+        
         for i in range(len(R)):
             intg = 1 - np.exp(-0.5*(R[i]/pmge["s"])**2)
             res[i] = 2*np.pi*np.sum(pmge["s"]**2*pmge["q"]*pmge["i"]*intg)
     
+    # within circular radius
     else:
-        # within circular radius
         for i in range(len(R)):
-            res[i] = 4*quad(lambda x: intg_projmass(x, R[i], pmge),
+            res[i] = 4*quad(lambda x: IntegrandCumulative2D(x, R[i], pmge),
                 0, np.pi/2)[0]*pmge["i"].unit*pmge["s"].unit**2
     
     return res
