7a38f03b6986bb8d5ba875ca5f7bbbe9f92a73da
Laura L Watkins <lauralwatkins@gmail.com>
2019-02-01 12:27:09 +0100

    Remove ellip, plot1d, plot1dmaj and plot2d functions.

--- __init__.py
+++ __init__.py
@@ -4,10 +4,6 @@ from .addbh import addbh
 from .Cumulative2D import Cumulative2D
 from .Cumulative3D import Cumulative3D
 from .Deproject import Deproject
-from .ellip import ellip
-from .plot1d import plot1d
-from .plot1dmaj import plot1dmaj
-from .plot2d import plot2d
 from .Project import Project
 from .SurfaceDensity import SurfaceDensity
 from .VolumeDensity import VolumeDensity
