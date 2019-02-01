a93b9b65dc9482ce0244457b233afe1925d1f03e
Laura L Watkins <lauralwatkins@gmail.com>
2019-02-01 12:22:51 +0100

    Remove qproj, qintr and qmed functions.

--- __init__.py
+++ __init__.py
@@ -9,8 +9,5 @@ from .plot1d import plot1d
 from .plot1dmaj import plot1dmaj
 from .plot2d import plot2d
 from .Project import Project
-from .qintr import qintr
-from .qmed import qmed
-from .qproj import qproj
 from .SurfaceDensity import SurfaceDensity
 from .VolumeDensity import VolumeDensity
