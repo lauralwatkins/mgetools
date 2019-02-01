43d6d0eedaf762ac827a57c8d2213592f114511d
Laura L Watkins <lauralwatkins@gmail.com>
2019-02-01 12:17:30 +0100

    Move projmass-->Cumulative2D and intrmass-->Cumulative3D.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 from .addbh import addbh
+from .Cumulative2D import Cumulative2D
+from .Cumulative3D import Cumulative3D
 from .Deproject import Deproject
 from .ellip import ellip
 from .incl import incl
-from .intrmass import intrmass
 from .plot1d import plot1d
 from .plot1dmaj import plot1dmaj
 from .plot2d import plot2d
 from .Project import Project
-from .projmass import projmass
 from .qintr import qintr
 from .qmed import qmed
 from .qproj import qproj
